d671be25abdb9c2665f04c45d6627c847e6ec03a
Pavel Jurca <paveljurca@users.noreply.github.com>
2017-09-10 20:43:34 +0200

    indentation saves lives

--- p2g_fix.pl
+++ p2g_fix.pl
@@ -12,38 +12,36 @@ p2g_fix(@ARGV);
 # ================
 
 sub p2g_fix {
+  while (defined(my $p2g = shift)) {
+    err(qq("$p2g" not a directory)) unless -d $p2g;
 
-# PERLDOC
-# Win32 users should use the real slash.
-# If you really want to use backslashes,
-# consider using Sarathy's File::DosGlob
+    # PERLDOC
+    # Win32 users should use the real slash.
+    # If you really want to use backslashes,
+    # consider using Sarathy's File::DosGlob
+    my ($xml) = grep(
+      /MediasitePresentation_\d{2}\.xml$/i,
+      bsd_glob(qq($p2g/*), File::Glob::GLOB_ERR | File::Glob::GLOB_NOSORT)
+    );
+    err(qq(Error reading "$p2g")) if File::Glob::GLOB_ERROR;
 
-while (defined(my $p2g = shift)) {
-err(qq("$p2g" not a directory)) unless -d $p2g;
+    local @ARGV = $xml || err(qq("$p2g" please specify P2G directory));
 
-my ($xml) = grep(
-/MediasitePresentation_\d{2}\.xml$/i,
-bsd_glob(qq($p2g/*), File::Glob::GLOB_ERR | File::Glob::GLOB_NOSORT)
-);
-err(qq(Error reading "$p2g")) if File::Glob::GLOB_ERROR;
+    # live_content_fix
+    my $fix = 0;
+    msg($p2g, 'found backup file, not proceeding'),next
+      if -e $xml.'.bak';
 
-local @ARGV = $xml || err(qq("$p2g" please specify P2G directory));
+    # in-place edit with a backup file
+    local $^I = '.bak';
+    while (<>) {
+      s/$1/false/,$fix++ if /<IsLive>(true)</i;
+      s/$1/0/,$fix++ if /<LiveStatus>(1)</i;
+      print;
+    }
 
-# live_content_fix
-my $fix = 0;
-msg($p2g, 'found backup file, not proceeding'),next
-  if -e $xml.'.bak';
-
-# in-place edit with a backup file
-local $^I = '.bak';
-while (<>) {
-s/$1/false/,$fix++ if /<IsLive>(true)</i;
-s/$1/0/,$fix++ if /<LiveStatus>(1)</i;
-print;
-}
-
-msg($p2g, $fix == 2 ? "Live Content fixed" : "OK");
-}
+    msg($p2g, $fix == 2 ? "Live Content fixed" : "OK");
+  }
 }
 
 sub msg {print shift, ' >> ', shift, "\n"}
